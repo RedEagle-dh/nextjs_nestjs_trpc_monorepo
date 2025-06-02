4e31e86e3316268962383e8f082bb25fc726c96a
David Hermann <redeagle.private@gmail.com>
2025-06-02 19:52:14 +0200

    Adding Dockerfile for frontend build

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,15 @@
+import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	output: "standalone",
+	webpack(config) {
+		config.resolve.alias["@mono/trpc/"] = path.resolve(
+			__dirname,
+			"../../packages/trpc",
+		);
+		return config;
+	},
 };
 
 export default nextConfig;
